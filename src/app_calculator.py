7b0f52574545f516707fb0e497c1d6581f0cda09
Nik Kozlov <nikkoz893@gmail.com>
2025-04-06 19:33:36 +0200

    fix: remove preloader

--- src/app_calculator.py
+++ src/app_calculator.py
@@ -3,7 +3,7 @@ import logging
 
 import dotenv
 
-from calculator.activities import find_potential_pairs_of_enhancers_promoters_for_project, calculate_distances_for_enhancer_promoters_chunk, upsert_project_configuration, persist_distances_for_enhancer_promoters_chunk, preload_datasets_for_project
+from calculator.activities import find_potential_pairs_of_enhancers_promoters_for_project, calculate_distances_for_enhancer_promoters_chunk, upsert_project_configuration, persist_distances_for_enhancer_promoters_chunk
 from calculator.workflows import CalculateDistancesForProjectWorkflow
 from utils.temporal_utils import get_temporal_worker
 
@@ -15,7 +15,6 @@ async def main() -> None:
     temporal_worker = await get_temporal_worker(
         task_queue="calculator-task-queue",
         activities=[
-            preload_datasets_for_project,
             upsert_project_configuration,
             find_potential_pairs_of_enhancers_promoters_for_project,
             calculate_distances_for_enhancer_promoters_chunk,
